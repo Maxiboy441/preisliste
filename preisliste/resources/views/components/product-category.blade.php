92748e0e0815a15dcde3fb0c08e846065fe25e10
Maximilian Huber <maximilian.huber@odt.net>
2024-06-07 09:30:19 +0200

    add margin top

--- preisliste/resources/views/components/product-category.blade.php
+++ preisliste/resources/views/components/product-category.blade.php
@@ -5,7 +5,7 @@
 
 @foreach(ProductTypeEnum::cases() as $type )
     @if(Product::where('type', $type)->count() > 0)
-        <h3 class="text-lg font-medium mb-2">{{$type}}</h3>
+        <h3 class="text-lg font-medium mb-2 mt-5">{{$type}}</h3>
     @endif
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach(Product::where('type', $type)->get() as $product)
